b3cd8d025f2c39fcb2042593b2ded82a0ebec7d7
MinhTri2204 <duongminhtri9311@gmail.com>
2025-12-13 17:52:18 +0700

    Add beautiful footer component for customer pages

--- frontend/src/layouts/CustomerLayout.jsx
+++ frontend/src/layouts/CustomerLayout.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import CustomerNavbar from '../components/CustomerNavbar'
+import Footer from '../components/Footer'
 
 export default function CustomerLayout() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 flex flex-col">
       <CustomerNavbar />
-      <main>
+      <main className="flex-grow">
         <Outlet />
       </main>
+      <Footer />
     </div>
   )
 }
